2db142fea226c3a1d74817f894edb7e69931f0e2
blueTimber <53584088+blueTimber@users.noreply.github.com>
2023-07-16 12:02:23 +0200

    Fix grammar and add shadow to highlight text

--- src/Components/Highlights.js
+++ src/Components/Highlights.js
@@ -106,7 +106,7 @@ function Highlights(props) {
     return (
         <div className='high-cont'>
             {SingleHighlight("Root-rot.png", "Game Jams", 
-                "I've participated in muliple game jams, which taught me how to work in a team and perform under pressure. My roles in these games varied from gameplay programmer, to technical artist, to 3D artist.", 
+                "I've participated in multiple game jams, which taught me how to work in a team and perform under pressure. My roles in these games varied from gameplay programmer, to technical artist, to 3D artist.", 
                 [["Puzzlegame.png", "https://zeyt8.itch.io/chalk-and-cheese"], 
                 ["Root-rot.png", "https://zeyt8.itch.io/root-rot"], 
                 ["SipOfLife.png", "https://zeyt8.itch.io/the-sip-of-life"]], 
